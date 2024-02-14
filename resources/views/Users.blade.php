0ad5042535071c19dffcd7ab0d966e8bf24c1e5e
Joshua Boloy <jbm5100@students.uc-bcf.edu.ph>
2024-02-14 08:55:57 +0800

    added a new laravel project named jash_app

---
+++ resources/views/Users.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Users</title>
+</head>
+<body>
+    <h1>NASA USERS PAGE KA NA! AYAW MO MANIWALA? EDI DON'T</h1>
+    <h1>LUHH AYAW TALAGA OHHHH</h1>
+    <h1>EDI WAG</h1>
+</body>
+</html>
